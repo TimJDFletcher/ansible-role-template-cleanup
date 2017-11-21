abc43fb96fc9f42f80b7b1575d21b810eb926767
Tim Fletcher <tim.fletcher@laterooms.com>
2017-11-21 13:52:42 +0000

    Detect not running on systemd and put a fake id out

--- files/firstboot.sh
+++ files/firstboot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo Initializing systemid $(hostname), generating /etc/machine-id
-/bin/systemd-machine-id-setup
+if [ -x /bin/systemd-machine-id-setup ] ; then 
+    echo Initializing systemid $(hostname), generating /etc/machine-id
+    /bin/systemd-machine-id-setup
+else
+    echo -n "fake-nosystemd-id" > /etc/machine-id
+fi
 
 /bin/run-parts /etc/firstboot.d/
